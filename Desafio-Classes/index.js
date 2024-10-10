3e1fb622b95fb3c82f729ab7713a1cb39446cd45
Breno Filho <128981128+Breno-Filho@users.noreply.github.com>
2024-10-09 23:59:51 -0300

    Add files via upload

---
+++ Desafio-Classes/index.js
@@ -0,0 +1,35 @@
+class hero {
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atack(){
+        
+        let ataque
+
+        if (this.tipo === "mago"){
+            ataque = "magia"
+        } else if (this.tipo === "guerreiro"){
+            ataque = "espada"
+        } else if (this.tipo === "monge"){
+            ataque = "artes marciais"
+        } else if (this.tipo === "ninja"){
+            ataque = "shuriken"
+        }
+
+        console.log(`O héroi de nome ${this.nome}, e tipo ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+let heroOne = new hero ("Breno", 20, "guerreiro")
+let HeroTwo = new hero ("Cristiano Ronaldo", 39, "ninja")
+let herothree = new hero("Lionel Messi", 37, "mago")
+let heroFour = new hero ("Neymar", 32, "monge")
+
+heroOne.atack();
+HeroTwo.atack();
+herothree.atack();
+heroFour.atack();
+
